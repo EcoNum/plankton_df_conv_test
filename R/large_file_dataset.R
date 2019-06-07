aa6a07e54203709598f7d8abb7d829bf0bd40f20
GuyliannEngels <guyliann.engels@umons.ac.be>
2019-06-07 13:04:05 +0200

    update large_file_dataset

--- R/large_file_dataset.R
+++ R/large_file_dataset.R
@@ -15,3 +15,16 @@ pb_download(file = "taxa.csv.gz",
             repo = "EcoNum/skimage-test",
             tag = "v1.0.0")
 
+pb_track("data/data_conv/")
+
+pb_upload(file = "data/data_raw/econum_set.fst",
+          repo = "Econum/plankton_df_conv_test",
+          tag = "v1.0.0",
+          name = "econum_data_test.fst",
+          overwrite = F)
+
+pb_upload(file = "data/data_conv/senoe_rev_var.fst",
+          repo = "Econum/plankton_df_conv_test",
+          tag = "v1.0.0",
+          name = "seanoe_var.fst",
+          overwrite = F)
